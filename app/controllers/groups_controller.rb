e8e1c967e26f7f69c15d17de789920473481500c
ryukenji0824 <ryukenji.411@icloud.com>
2020-02-27 15:37:32 +0900

    Update groups_controller.rb
    before_actionでメソッドで切り分けたコードを反映
    
    set_groupと定義したメソッドをeditとupdateアクションで使用

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController  
-  
+  before_action :set_group, only: [:edit,:update]
   def index
   end
 
@@ -16,15 +16,11 @@ class GroupsController < ApplicationController
       render :new
     end
   end
-  def group
-    @group = Group.find(params[:id])
-  end
+  
     def edit
-      group
     end
 
     def update
-      group
       if @group.update(group_params)
         redirect_to root_path, notice: 'グループを更新しました'
       else
@@ -35,5 +31,9 @@ class GroupsController < ApplicationController
     private
   def group_params
     params.require(:group).permit(:name, user_ids: [])
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
