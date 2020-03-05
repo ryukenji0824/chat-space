3898df98bca39e3c50612774ccc047cce1ea4507
ryukenji0824 <ryukenji.411@icloud.com>
2020-03-05 11:00:08 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,66 +1,48 @@
-$(function(){
-  function buildHTML(message){
-    if ( message.image ) {
-      var html =
-       `<div class="message">
+$(function () {
+
+  function buildHTML(message) {
+
+    image = (message.image) ? `<img class= "lower-message__image" src=${message.image} >` : ""; 
+    var html = `<div class="message" data-message-id="${message.id}"> 
           <div class="upper-message">
             <div class="upper-message__user-name">
               ${message.user_name}
             </div>
             <div class="upper-message__date">
-              ${message.created_at}
+              ${message.date}
             </div>
           </div>
-          <div class="lower-message">
+          <div class="lower-meesage">
             <p class="lower-message__content">
               ${message.content}
             </p>
-          </div>
-          <img class="lower-message__image" src="${message.image}" alt="Arrow top">
-        </div>`
-      return html;
-    } else {
-      var html =
-       `<div class="message">
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.created_at}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
+            ${image}
           </div>
         </div>`
-      return html;
-    };
+    return html;
   }
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-      var formData = new FormData(this);
-      var url = $(this).attr('action');
-      $.ajax({
-        url: url,
-        type: "POST",
-        data: formData,
-        dataType: 'json',
-        processData: false,
-        contentType: false
+  }) 
+  var reloadMessages = function () {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.message:last').data("message-id"); 
+
+      $.ajax({ 
+        url: "api/messages", 
+        type: 'get', 
+        dataType: 'json', 
+        data: {last_id: last_message_id} 
       })
-      .done(function(data){
-        var html = buildHTML(data);
-        $('.messages').append(html);
-        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight},'fast');
-        $('.form__submit').prop('disabled', false);
-        $('form')[0].reset();
+      .done(function (messages) { 
+        var insertHTML = '';
+        messages.forEach(function (message) {
+          insertHTML = buildHTML(message); 
+          $('.messages').append(insertHTML);
+        })
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
       })
-      .fail(function() {
-        alert("メッセージ送信に失敗しました");
+      .fail(function () {
+        alert('自動更新に失敗しました');
       });
-      return false;
-    });
-  });
+    }
+  };
+  setInterval(reloadMessages, 5000);
