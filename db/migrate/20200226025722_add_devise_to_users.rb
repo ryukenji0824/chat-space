10647bc83a821911b7050266564e0e081809fb85
ryukenji0824 <ryukenji.411@icloud.com>
2020-03-06 16:19:51 +0900

    modified in db

--- db/migrate/20200226025722_add_devise_to_users.rb
+++ db/migrate/20200226025722_add_devise_to_users.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class AddDeviseToUsers < ActiveRecord::Migration[5.0]
-  def self.up
-    change_table :users do |t|
+  def change
+    create_table :users do |t|
       t.string :name, null: false
-      t.string :email,              null: false, default: ""
+      t.string :email,null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
@@ -45,9 +45,9 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.0]
     # add_index :users, :unlock_token,         unique: true
   end
 
-  def self.down
-    # By default, we don't want to make any assumption about how to roll back a migration when your
-    # model already existed. Please edit below which fields you would like to remove in this migration.
-    raise ActiveRecord::IrreversibleMigration
-  end
+  # def self.down
+  #   # By default, we don't want to make any assumption about how to roll back a migration when your
+  #   # model already existed. Please edit below which fields you would like to remove in this migration.
+  #   raise ActiveRecord::IrreversibleMigration
+  # end
 end
